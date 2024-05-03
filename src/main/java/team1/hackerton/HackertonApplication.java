25ef78d773ec3c03e3c43c9e96b5050ae385b58b
hyeon-gyu <hyungue729@naver.com>
2024-05-03 20:02:51 +0900

    first commit

---
+++ src/main/java/team1/hackerton/HackertonApplication.java
@@ -0,0 +1,13 @@
+package team1.hackerton;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackertonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackertonApplication.class, args);
+	}
+
+}
